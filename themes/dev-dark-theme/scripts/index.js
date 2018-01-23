1290d20e734bc600e4ae41759b17d665ddc893ab
Marcos Carvalho <mcarvalho@allin.com.br>
2018-01-23 11:51:04 -0200

    adding rss

--- themes/dev-dark-theme/scripts/index.js
+++ themes/dev-dark-theme/scripts/index.js
@@ -1,16 +1,16 @@
-hexo.extend.helper.register('url_categories', args => {
-  let original = args;
-  let categories = [];
 
-  original.data.forEach(element => {
-    categories.push(element.name.replace(new RegExp(' ', 'g'), '-').toLowerCase());
+hexo.extend.helper.register('url_data', args => {
+  let data = [];
+
+  args.data.forEach(element => {
+    data.push(element.name.replace(new RegExp(' ', 'g'), '-'));
   });
 
-  return categories.join('/');
+  return data.join('/').toLowerCase();
 });
-hexo.extend.helper.register('first_category', args => {
-  let category = args.data[0] ? args.data[0].name : '';
-  return category.replace(new RegExp(' ', 'g'), '-').toLowerCase();
+hexo.extend.helper.register('first_data', args => {
+  let data = args.data[0] ? args.data[0].name : '';
+  return data.replace(new RegExp(' ', 'g'), '-').toLowerCase();
 });
 hexo.extend.helper.register('url_title', args => {
   let original = args;
